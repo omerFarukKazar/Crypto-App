e385cef6390997cf953c0937764c870395ea8126
omerfkzr <omer.kazar22@gmail.com>
2023-01-18 07:29:15 +0300

    ViewModel is created. Network calls and responses handled. ViewModel is injected to ViewController by using 'init Dependency Injection'

--- Crypto App/Crypto App/Screens/CryptoList/Controller/CryptoListViewController.swift
+++ Crypto App/Crypto App/Screens/CryptoList/Controller/CryptoListViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class CryptoListViewController: UIViewController {
-
+    // MARK: - Xib Elements
     @IBOutlet private weak var tableView: UITableView!
 
+    // MARK: - Properties
+    private let viewModel: CryptoListViewModel // Dependency Injection
+
+    // MARK: - Init
+    init(viewModel: CryptoListViewModel) { // Dependency Injection
+        self.viewModel = viewModel
+        super.init(nibName: "CryptoListViewController", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        viewModel.fetchCoins() // Try to fetch Coins
+        viewModel.changeHandler = { change in // Take action by the result
+            switch change {
+            case .didFetchCoins:
+                self.tableView.reloadData()
+            case .didErrorOccured(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 
 }
@@ -26,12 +51,12 @@ extension CryptoListViewController: UITableViewDelegate {
 // MARK: - UITableViewDataSource
 extension CryptoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        viewModel.numberOfRows
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Cell - \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel.coinForIndexPath(indexPath)?.name
         return cell
     }
 
---
+++ Crypto App/Crypto App/Screens/CryptoList/Model/Coins.swift
@@ -0,0 +1,36 @@
+//
+//  Coins.swift
+//  Crypto App
+//
+//  Created by Ömer Faruk Kazar on 15.01.2023.
+//
+
+import Foundation
+
+struct Coin: Decodable {
+    let id: String?
+    let icon: String?
+    let name, symbol: String?
+    let rank: Int?
+    let price, priceBtc: Double?
+    let volume: Double?
+    let marketCap, availableSupply, totalSupply, priceChange1H: Double?
+    let priceChange1D, priceChange1W: Double?
+    let websiteURL: String?
+    let twitterURL: String?
+    let exp: [String]?
+    let contractAddress: String?
+    let decimals: Int?
+    let redditURL: String?
+
+//    enum CodingKeys: String, CodingKey {
+//        case id, icon, name, symbol, rank, price, priceBtc, volume, marketCap, availableSupply, totalSupply
+//        case priceChange1H = "priceChange1h"
+//        case priceChange1D = "priceChange1d"
+//        case priceChange1W = "priceChange1w"
+//        case websiteURL = "websiteUrl"
+//        case twitterURL = "twitterUrl"
+//        case exp, contractAddress, decimals
+//        case redditURL = "redditUrl"
+//    }
+}
