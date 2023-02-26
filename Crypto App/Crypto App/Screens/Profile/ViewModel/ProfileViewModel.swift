479ceb5cb89e0173a82c91b2d219b36a7b93f2cd
omerfkzr <omer.kazar22@gmail.com>
2023-02-26 23:09:11 +0300

    Add FetchUser, add UserDataSetter protocol. add fetchUser() function to fetch user data and update eMailLabel on the view. add UserDataSetter protocol to pass variables to ProfileViewController

--- Crypto App/Crypto App/Screens/Profile/ViewModel/ProfileViewModel.swift
+++ Crypto App/Crypto App/Screens/Profile/ViewModel/ProfileViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseStorage
+
+protocol UserDataSetter: AnyObject {
+    func didGetUserMail(mail: String)
+}
+
+final class ProfileViewModel: CAViewModel {
+
+    // MARK: - Properties
+    // swiftlint:disable:next identifier_name
+    private let db = Firestore.firestore()
+    private let storage = Storage.storage().reference()
+    weak var dataDelegate: UserDataSetter?
+    // MARK: - Methods
+    /// Fetches User data on FirebaseFirestore and sets the delegate's mail property with user's mail on database.
+    func fetchUser() {
+        guard let uid = uid else { return }
+
+        db.collection("users").document(uid).getDocument { querySnapshot, _ in
+            guard let data = querySnapshot?.data() else { return }
+            let user = User(from: data)
+
+            guard let eMail = user.email else { return }
+            self.dataDelegate?.didGetUserMail(mail: eMail)
+        }
+    }
+}
