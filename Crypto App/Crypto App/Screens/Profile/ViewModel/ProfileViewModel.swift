6429dd3b3758e96ebee13fa9acbb68b27baadc02
omerfkzr <omer.kazar22@gmail.com>
2023-02-26 23:13:47 +0300

    Add fetchProfilePhoto() and uploadProfilePhoto() functions to ProfileViewModel Add fetchProfilePhoto() function to fetch latest profile photo from FirebaseStore to compare it to the one user picked from gallery. Add uploadProfilePhoto() function to upload selected photo to FirebaseStore if the photo isn't same with the last profile photo. Add two new methods to UserDataSetter protocol

--- Crypto App/Crypto App/Screens/Profile/ViewModel/ProfileViewModel.swift
+++ Crypto App/Crypto App/Screens/Profile/ViewModel/ProfileViewModel.swift
@@ -11,6 +11,8 @@ import FirebaseStorage
 
 protocol UserDataSetter: AnyObject {
     func didGetUserMail(mail: String)
+    func didGetUserImageData(data: Data)
+    func didErrorOccured(error: Error)
 }
 
 final class ProfileViewModel: CAViewModel {
@@ -31,6 +33,35 @@ final class ProfileViewModel: CAViewModel {
 
             guard let eMail = user.email else { return }
             self.dataDelegate?.didGetUserMail(mail: eMail)
+        }
+    }
+    /// Fetches Profile Photo  on FirebaseFirestore and sets the delegate's mail property with user's mail on database.
+    func fetchProfilePhoto() {
+        guard let urlString = UserDefaults.standard.value(forKey: "profilePhotoUrl") as? String,
+        let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            self.dataDelegate?.didGetUserImageData(data: data)
+        }.resume()
+    }
+    /// upload profile photo to FirebaseStorage and set image's url to UserDefaults
+    ///  - parameters:
+    ///     - imageData: The data of picked image that comes from imagePickerController
+    func uploadProfilePhoto(with imageData: Data) throws {
+        guard let uid = uid else { return }
+
+        storage.child("profilePhotos/").child("\(uid)/profilePhoto.png").putData(imageData) { _, error in
+            if let error = error {
+                self.dataDelegate?.didErrorOccured(error: error)
+            } else {
+                self.storage.child("profilePhotos/").child("\(uid)/profilePhoto.png").downloadURL { url, error in
+                    guard let url = url, error == nil else { return }
+
+                    let urlString = url.absoluteString
+                    UserDefaults.standard.set(urlString, forKey: "profilePhotoUrl")
+                }
+            }
         }
     }
 }
